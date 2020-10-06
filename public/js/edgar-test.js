5e603a44b52f0c8ebdd0bc3c816dbbd82acc0cab
Edgar Galvan-Cuesta <edgargalvan-cuesta@Edgars-MacBook-Pro-2.local>
2020-10-06 18:05:21 -0500

    added some views as components

---
+++ public/js/edgar-test.js
@@ -0,0 +1,42 @@
+// var socket = io();
+
+// class Edgar extends React.Component {
+//     state = {
+//       // socket: io()
+//       newMessage: ""
+//     };
+//     componentDidMount = () =>{
+//       socket.on('chatid', function(msg){
+//         console.log("I am here")
+//         $('#messages').append($('<div class="msg">').text(msg));
+//       })
+//     }
+//     handleChange= (event) =>{
+//       this.setState({
+//         // [event.target.id]: event.target.value
+//       })
+//     }
+//     sendMessage = (e) =>{
+//       // e.preventDefault(); // prevents page reloading
+//       // socket.emit('test', $('#m').val());
+//       // e.currentTarget.reset();
+//     }
+//     updateMessage = (nm) =>{
+//       this.setState({
+//         // newMessage: nm
+//       })
+//     }
+//     render = () => {
+//       return <div id="edgars-main-div">
+//         {/* <div id="messages">
+//         {this.state.newMessage}
+//         </div>
+//         <form onSubmit={this.sendMessage}>
+//           <input type="text" id="m" onChange={this.handleChange}/>
+//           <input type="submit"/>
+//         </form> */}
+//       </div>;
+//     };
+//   }
+  
+//   ReactDOM.render(<Edgar />, document.querySelector(".edgar-js"));
--- public/js/edgar.js
+++ public/js/edgar.js
@@ -1,45 +1,97 @@
-// const { set } = require("mongoose");
 
-var socket = io();
+  class Login extends React.Component{
+    // state = {
+    //   username: "",
+    //   password: ""
+    // }
+    handleChange= (event) =>{
+      this.setState({
+        [event.target.id]: event.target.value
+      })
+    }
+    submitCreds = (event) =>{
+      console.log("we are submitting creds")
+    }
+    render = () =>{
+      <form action="/sessions" method="POST">
+        <label for="name">User Name:</label>
+        <input type="text" name="username" required/>
+        <label for="color">Password:</label>
+        <input type="password" name="password" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    }
+  }
+
+  class Signup extends React.Component{
+    // state = {
+    //   username: "",
+    //   password: "",
+    //   confirmPW: ""
+    // }
+    handleChange= (event) =>{
+      this.setState({
+        [event.target.id]: event.target.value
+      })
+    }
+    submitCreds = (event) =>{
+      console.log("we are submitting creds")
+    }
+    render = () =>{
+      <form action="/users" method="POST">
+        <label for="name">User Name:</label>
+        <input type="text" name="username" required/>
+        <label for="color">Password:</label>
+        <input type="password" name="password" />
+        {/* <label for="confirmPW">Confirm Password</label>
+        <input type="password" name="confirmPW"/> */}
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    }
+  }
+  
 
 class Edgar extends React.Component {
     state = {
-      // socket: io()
-      newMessage: ""
+      currently: 0 // 0 = not logged in ; 1 = log in ; 2 = signing up; 3 = log out
     };
-    componentDidMount = () =>{
-      socket.on('chatid', function(msg){
-        console.log("I am here")
-        $('#messages').append($('<div class="msg">').text(msg));
-      })
+    ComponentDidMount = () =>{
+      // this.setState({
+      //   currently: 0
+      // });
     }
+    // run = () =>{
+      // switch(this.state.currently){
+      //   case 0: return (
+      //     <div>
+      //       <button className="log-in auth-button" onClick={this.updateCurrently(1)}>LOG IN</button>
+      //       <button className="sign-up auth-button" onClick={this.updateCurrently(2)}>SIGN UP</button>
+      //       <button className="log-out auth-button" onClick={this.updateCurrently(0)}>LOG OUT</button>
+      //     </div>)
+      //   case 1: return <Login></Login>
+      //   case 2: return <Signup></Signup>
+      // }
+    // }
     handleChange= (event) =>{
       this.setState({
         // [event.target.id]: event.target.value
       })
     }
-    sendMessage = (e) =>{
-      // e.preventDefault(); // prevents page reloading
-      // socket.emit('test', $('#m').val());
-      // e.currentTarget.reset();
-    }
-    updateMessage = (nm) =>{
+    updateCurrently = (x) =>{
       this.setState({
-        // newMessage: nm
+        currently: x
       })
     }
     render = () => {
       return <div id="edgars-main-div">
-        {/* <div id="messages">
-        {this.state.newMessage}
-        </div>
-        <form onSubmit={this.sendMessage}>
-          <input type="text" id="m" onChange={this.handleChange}/>
-          <input type="submit"/>
-        </form> */}
+            {/* <button className="log-in auth-button" onClick={this.updateCurrently(1)}>LOG IN</button>
+            <button className="sign-up auth-button" onClick={this.updateCurrently(2)}>SIGN UP</button>
+            <button className="log-out auth-button" onClick={this.updateCurrently(0)}>LOG OUT</button> */}
       </div>;
     };
   }
-  
+
   ReactDOM.render(<Edgar />, document.querySelector(".edgar-js"));
   
